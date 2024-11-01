e77774554cfcb3a611af8abdd4adea8e9aae1f3c
Tim Kuntz <tim.kuntz@appfolio.com>
2024-11-01 17:39:19 -0500

    Add keymap for tpope rails go to alternate

--- nvim/dot-config/nvim/lua/custom/plugins/ruby.lua
+++ nvim/dot-config/nvim/lua/custom/plugins/ruby.lua
@@ -3,5 +3,11 @@ return {
 	dependencies = {
 		'tpope/vim-bundler',
 		'vim-ruby/vim-ruby',
-	}
+	},
+	config = function()
+		local keymap = {
+      { "ga", "<cmd>:A<cr>", desc = "go [a]ternate", nowait = false, remap = false },
+    }
+    require("which-key").add(keymap)
+	end
 }
