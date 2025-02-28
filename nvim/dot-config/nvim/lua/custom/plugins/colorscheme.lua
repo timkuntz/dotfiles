04ad052b496d8a028acd722cc9c13f5981a615a3
Tim Kuntz <tim.kuntz@appfolio.com>
2025-02-28 14:39:59 -0600

    Don't use different colorscheme for markdown and help

--- nvim/dot-config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/custom/plugins/colorscheme.lua
@@ -5,8 +5,8 @@ return {
     config = function()
       require("styler").setup {
         themes = {
-          markdown = { colorscheme = "tokyonight" },
-          help = { colorscheme = "tokyonight" },
+          -- markdown = { colorscheme = "tokyonight" },
+          -- help = { colorscheme = "tokyonight" },
         },
       }
     end,
@@ -23,6 +23,19 @@ return {
         sidebars = "transparent",
         floats = "transparent",
       },
+      -- https://github.com/folke/tokyonight.nvim/blob/main/extras/lua/tokyonight_night.lua
+      on_colors = function(colors)
+        -- colors.bg = "#1a1b26" -- default
+        -- colors.bg = "#000000"
+      end,
+      on_highlights = function(highlights, _)
+        -- highlights.LineNr = { fg = "#3b4261" } -- default
+        highlights.LineNr = { fg = "#6e7cb7" }
+        -- highlights.LineNrAbove = "#3b4261"
+        highlights.LineNrAbove = { fg = "#6e7cb7" }
+        -- highlights. LineNrBelow =  "#3b4261"
+        highlights. LineNrBelow = { fg =  "#6e7cb7" }
+      end,
     },
     config = function(_, opts)
       local tokyonight = require "tokyonight"
