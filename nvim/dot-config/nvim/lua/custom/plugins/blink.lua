c38ebb98862804d89ead26a6b89ca2a9cbba3f2f
Tim Kuntz <tim.kuntz@appfolio.com>
2025-02-28 14:43:35 -0600

    Customize blink

--- nvim/dot-config/nvim/lua/custom/plugins/blink.lua
+++ nvim/dot-config/nvim/lua/custom/plugins/blink.lua
@@ -3,7 +3,7 @@ return {
 
   enabled = true,
   -- optional: provides snippets for the snippet source
-  -- dependencies = 'rafamadriz/friendly-snippets',
+  dependencies = 'rafamadriz/friendly-snippets',
 
   -- use a release tag to download pre-built binaries
   version = '*',
@@ -17,7 +17,14 @@ return {
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = 'default' },
+    keymap = {
+      preset = 'default',
+      -- cmdline = {
+      --   preset = 'default',
+      --   ['<S-Tab>'] = { 'select_prev', 'fallback' },
+      --   ['<Tab>'] = { 'select_next', 'fallback' },
+      -- }
+    },
 
     appearance = {
       -- Sets the fallback highlight groups to nvim-cmp's highlight groups
@@ -29,10 +36,13 @@ return {
       nerd_font_variant = 'mono'
     },
 
+    signature = { enabled = true },
+
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
+      -- cmdline = {},
       providers = {
         lazydev = {
           name = "LazyDev",
